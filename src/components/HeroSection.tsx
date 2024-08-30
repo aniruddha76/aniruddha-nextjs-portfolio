23be7ee719f7c77f3ec5eae3295e628bafbbd183
aniruddha <aniruddhathakur76@gmail.com>
2024-08-30 18:16:27 +0530

    Fixed &apos; error in Let's talk Button

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -24,7 +24,7 @@ export default function HeroSection() {
             <div className="flex gap-2 z-10">
                 <Link href={'mailto:aniruddhathakur76@gmail.com'} target="_blank" passHref>
                 <button className="inline-flex h-12 w-40 animate-shimmer items-center justify-center rounded-xl border border-white bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-all hover:scale-110 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                    Let's Talk
+                    Let&apos;s Talk
                 </button>
                 </Link>
 
